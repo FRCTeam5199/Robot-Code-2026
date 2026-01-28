975befd7afc92f7668b38b2840c282b5f64ea1e7
Rohan-5199 <rohan745patel@gmail.com>
2026-01-27 17:57:42 -0800

    forgot to adjust constants

--- src/main/java/frc/robot/constants/IndexerConstants.java
+++ src/main/java/frc/robot/constants/IndexerConstants.java
@@ -4,13 +4,13 @@ import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
 
 public class IndexerConstants {
-    public static final int INDEXER_ROLLER_MOTOR_ID = 15;
-    public static final double INDEXER_ROLLER_STATOR_CURRENT_LIMIT = 60;
-    public static final double INDEXER_ROLLER_SUPPLY_CURRENT_LIMIT = 60;
-    public static final boolean INDEXER_ROLLER_INVERTED = true;
-    public static final boolean INDEXER_ROLLER_BRAKE = true;
+    public static final int INDEXER_MOTOR_ID = 15;
+    public static final double INDEXER_STATOR_CURRENT_LIMIT = 60;
+    public static final double INDEXER_SUPPLY_CURRENT_LIMIT = 60;
+    public static final boolean INDEXER_INVERTED = true;
+    public static final boolean INDEXER_BRAKE = true;
 
-    public static final Slot0Configs INDEXER_ROLLER_SLOT0_CONFIGS = new Slot0Configs()
+    public static final Slot0Configs INDEXER_SLOT0_CONFIGS = new Slot0Configs()
             .withKP(0)
             .withKI(0)
             .withKD(0)
@@ -20,11 +20,11 @@ public class IndexerConstants {
             .withKA(0)
             .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseVelocitySign);
 
-    public static final double INDEXER_ROLLER_ACCELERATION = 400;
-    public static final double INDEXER_ROLLER_JERK = 4000;
+    public static final double INDEXER_ACCELERATION = 400;
+    public static final double INDEXER_JERK = 4000;
 
-    public static final double INDEXER_ROLLER_LOWER_TOLERANCE = 2;
-    public static final double INDEXER_ROLLER_UPPER_TOLERANCE = 2;
+    public static final double INDEXER_LOWER_TOLERANCE = 2;
+    public static final double INDEXER_UPPER_TOLERANCE = 2;
 
-    public static final double[][] INDEXER_ROLLER_GEAR_RATIO = {{1, 1}};
+    public static final double[][] INDEXER_GEAR_RATIO = {{1, 1}};
 }
--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems;
 
-import frc.robot.constants.IntakeRollerConstants;
+import frc.robot.constants.IndexerConstants;
 import frc.robot.subsystems.templates.TemplateSubsystem;
 import frc.robot.utility.Type;
 
@@ -8,17 +8,17 @@ public class IndexerSubsystem extends TemplateSubsystem {
     private static IndexerSubsystem indexerSubsystem;
 
     private IndexerSubsystem() {
-        super(Type.ROLLER, IntakeRollerConstants.INTAKE_ROLLER_MOTOR_ID,
-                0, IntakeRollerConstants.INTAKE_ROLLER_ACCELERATION,
-                IntakeRollerConstants.INTAKE_ROLLER_JERK,
-                IntakeRollerConstants.INTAKE_ROLLER_LOWER_TOLERANCE,
-                IntakeRollerConstants.INTAKE_ROLLER_UPPER_TOLERANCE,
-                IntakeRollerConstants.INTAKE_ROLLER_GEAR_RATIO, "INTAKE_ROLLER");
+        super(Type.ROLLER, IndexerConstants.INDEXER_MOTOR_ID,
+                0, IndexerConstants.INDEXER_ACCELERATION,
+                IndexerConstants.INDEXER_JERK,
+                IndexerConstants.INDEXER_LOWER_TOLERANCE,
+                IndexerConstants.INDEXER_UPPER_TOLERANCE,
+                IndexerConstants.INDEXER_GEAR_RATIO, "INDEXER");
 
-        configureMotor(IntakeRollerConstants.INTAKE_ROLLER_INVERTED, IntakeRollerConstants.INTAKE_ROLLER_BRAKE,
-                IntakeRollerConstants.INTAKE_ROLLER_SUPPLY_CURRENT_LIMIT,
-                IntakeRollerConstants.INTAKE_ROLLER_STATOR_CURRENT_LIMIT,
-                IntakeRollerConstants.INTAKE_ROLLER_SLOT0_CONFIGS);
+        configureMotor(IndexerConstants.INDEXER_INVERTED, IndexerConstants.INDEXER_BRAKE,
+                IndexerConstants.INDEXER_SUPPLY_CURRENT_LIMIT,
+                IndexerConstants.INDEXER_STATOR_CURRENT_LIMIT,
+                IndexerConstants.INDEXER_SLOT0_CONFIGS);
     }
 
     public static IndexerSubsystem getInstance() {
