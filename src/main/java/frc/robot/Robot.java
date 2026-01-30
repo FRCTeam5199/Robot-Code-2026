233c64796958e62872cf81f9ae76391a4e092ecd
Leo Sent <leonardosent@gmail.com>
2026-01-29 16:18:54 -0800

    Improvements

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,21 +4,23 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.HootAutoReplay;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.IntakePivotSubsystem;
+import frc.robot.subsystems.IntakeRollerSubsystem;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
 
     private final RobotContainer m_robotContainer;
+    // private final IntakeRollerSubsystem intakeRollerSubsystem = IntakeRollerSubsystem.getInstance();
+    // private final IntakePivotSubsystem intakePivotSubsystem = IntakePivotSubsystem.getInstance();
 
-    /* log and replay timestamp and joystick data */
-    private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
-        .withTimestampReplay()
-        .withJoystickReplay();
+    // /* log and replay timestamp and joystick data */
+    // private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
+    //     .withTimestampReplay()
+    //     .withJoystickReplay();
 
     public Robot() {
         m_robotContainer = new RobotContainer();
@@ -31,7 +33,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotPeriodic() {
-        m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run();
     }
 
@@ -68,16 +69,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        if (!UserInterface.controlComponents.get("Edit Button").getBoolean(false)) {
-            UserInterface.controlComponents.get("Actuator Percent").setDouble(/*motor.get()*/0.0);
-            UserInterface.controlComponents.get("Actuator Voltage").setDouble(/*motor.get()*/0.0);
-            UserInterface.controlComponents.get("Actuator Velocity").setDouble(/*motor.get()*/0.0);
-            UserInterface.controlComponents.get("Actuator Position").setDouble(/*motor.get()*/0.0);
-        } else {
-            while (UserInterface.controlComponents.get("Edit Button").getBoolean(false)) {}
-
-            //motor.set(UserInterface.controlComponents.get("ActuatorPercent").getDouble(motor.get());
-        }
+        
     }
 
     @Override
--- src/main/java/frc/robot/UserInterface.java
+++ src/main/java/frc/robot/UserInterface.java
@@ -3,55 +3,59 @@ package frc.robot;
 import java.util.HashMap;
 import java.util.Map;
 
-import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableValue;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 import edu.wpi.first.wpilibj.shuffleboard.WidgetType;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.utility.Elastic;
 
 public class UserInterface {
-    private static ShuffleboardTab autonTab, teleopTab, controlTab;
-
-    public static Map<String, GenericEntry> autonComponents = new HashMap<String, GenericEntry>();
-    public static Map<String, GenericEntry> teleopComponents = new HashMap<String, GenericEntry>();
-    public static Map<String, GenericEntry> controlComponents = new HashMap<String, GenericEntry>();
+    public static Map<String, SimpleWidget> components = new HashMap<String, SimpleWidget>();
 
     private UserInterface() {}
 
     public static void init() {
-        initalizeTabs();
-        initalizeComponents();
+        createComponent("Pivot Percent", "Control", BuiltInWidgets.kTextView, 0, 0, 1, 1);
+        createComponent("Pivot Position", "Control", BuiltInWidgets.kTextView, 1, 0, 1, 1);
+        createComponent("Pivot Velocity", "Control", BuiltInWidgets.kTextView, 2, 0, 1, 1);
+        createComponent("Pivot Voltage", "Control", BuiltInWidgets.kVoltageView, 3, 0, 1, 1);
+
+        // createComponent("Pivot Percent", "Control", BuiltInWidgets.kTextView, 0, 0, 1, 1);
+        // createComponent("Pivot Velocity", "Control", BuiltInWidgets.kTextView, 0, 0, 1, 1);
+        // createComponent("Pivot Voltage", "Control", BuiltInWidgets.kTextView, 0, 0, 1, 1);
+
         Shuffleboard.selectTab("Autons");
-    }
-
-    private static void initalizeTabs() {
-        autonTab = Shuffleboard.getTab("Auton");
-        teleopTab = Shuffleboard.getTab("Teleop");
-        controlTab = Shuffleboard.getTab("Control");
-    }
-
-    private static void initalizeComponents() {
-        createControlComponent("Actuator Percent", 0.0d, BuiltInWidgets.kTextView, 0, 0, 2, 2, null);
-        createControlComponent("Actuator Voltage", 0.0d, BuiltInWidgets.kTextView, 0, 2, 2, 2, null);
-        createControlComponent("Actuator Velocity", 0.0d, BuiltInWidgets.kTextView, 0, 4, 2, 2, null);
-        createControlComponent("Actuator Position", 0.0d, BuiltInWidgets.kTextView, 0, 6, 2, 2, null);
-
-        createControlComponent("Roller Percent", 0.0d, BuiltInWidgets.kTextView, 2, 0, 2, 2, null);
-        createControlComponent("Roller Voltage", 0.0d, BuiltInWidgets.kTextView, 2, 2, 2, 2, null);
-        createControlComponent("Roller Velocity", 0.0d, BuiltInWidgets.kTextView, 2, 4, 2, 2, null);
-
-        createControlComponent("Edit Button", false, BuiltInWidgets.kToggleSwitch, 5, 0, 1, 1, null);
+        Elastic.selectTab("Autons");
     }
 
     public static void setTab(String key) {
         Shuffleboard.selectTab(key);
+        Elastic.selectTab(key);
     }
-    
+
     public static ShuffleboardTab getTab(String key) {
         return Shuffleboard.getTab(key);
+    }
+
+    public static SimpleWidget createComponent(String name, String tab, WidgetType widgetType, int posx, int posy, int sizex, int sizey) {
+        return components.putIfAbsent(name, Shuffleboard.getTab(tab)
+            .add(name, Sendable.class)
+            .withWidget(widgetType)
+            .withPosition(posx, posy)
+            .withSize(sizex, sizey));
+    }
+
+    public static void deleteComponent(String name) {
+        components.get(name).close();
+        components.remove(name);
+    }
+
+    public static void setComponentValue(String name, NetworkTableValue data) {
+        components.get(name).getEntry().set(data);
     }
 
     public static void putData(String key, Sendable data) {
@@ -60,51 +64,6 @@ public class UserInterface {
 
     public static Sendable getData(String key) {
         return SmartDashboard.getData(key);
-    }
-
-    @SuppressWarnings("rawtypes")
-    public static void addSendableChooserComponent(String name, SendableChooser sendableChooser, WidgetType widget, int positionX, int positionY, int width, int height, Map<String, Object> properties) {
-        autonTab.add(name, sendableChooser).withWidget(widget).withPosition(positionX, positionY).withSize(width, height).withProperties(properties);
-    }
-
-    public static void createAutonComponent(String name, Object defaultValue, WidgetType widget, int positionX, int positionY, int width, int height, Map<String, Object> properties) {
-        autonComponents.putIfAbsent(name, autonTab.add(name, defaultValue).withWidget(widget).withPosition(positionX, positionY).withSize(width, height).withProperties(properties).getEntry());
-    }
-
-    public static void createTeleopComponent(String name, Object defaultValue, WidgetType widget, int positionX, int positionY, int width, int height, Map<String, Object> properties) {
-        teleopComponents.putIfAbsent(name, teleopTab.add(name, defaultValue).withWidget(widget).withPosition(positionX, positionY).withSize(width, height).withProperties(properties).getEntry());
-    }
-
-    public static void createControlComponent(String name, Object defaultValue, WidgetType widget, int positionX, int positionY, int width, int height, Map<String, Object> properties) {
-        controlComponents.putIfAbsent(name, controlTab.add(name, defaultValue).withWidget(widget).withPosition(positionX, positionY).withSize(width, height).withProperties(properties).getEntry());
-    }
-
-    public static GenericEntry getAutonComponent(String key) {
-        return autonComponents.get(key);
-    }
-
-    public static GenericEntry getTeleopComponent(String key) {
-        return teleopComponents.get(key);
-    }
-
-    public static GenericEntry getControlComponent(String key) {
-        return controlComponents.get(key);
-    }
-
-    public static void setAutonComponent(String key, Object value) {
-        if (autonComponents.get(key).setValue(value)) {
-            autonComponents.replace(key, autonComponents.get(key));
-        }
-    }
-
-    public static void setTeleopComponent(String key, Object value) {
-        if (teleopComponents.get(key).setValue(value)) {
-            teleopComponents.replace(key, autonComponents.get(key));
-        }
-    }
-
-    public static void setControlComponent(String key, Object value) {
-        controlComponents.get(key).setValue(value);
     }
 
     public static void update() {
