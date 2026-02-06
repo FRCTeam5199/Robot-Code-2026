41f72ae22a75137ec35acc1ebeee8b2c0a3be77e
Cydaguy <themrbestgamer@gmail.com>
2026-02-05 20:57:33 -0800

    Kicker Command Changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -133,8 +133,14 @@ public class RobotContainer {
                                 new PositionCommand(hoodSubsystem, Setpoint.LEFT_CORNER.getHoodAngle()),
                                 new VelocityCommand(shooterSubsystem, Setpoint.LEFT_CORNER.getShooterSpeed())
                         ))
-                ), RobotContainer::getCurrentSetpoint).alongWith(new VelocityCommand(kickerSubsystem, 30))
-        ).onFalse(
+                ), RobotContainer::getCurrentSetpoint).alongWith(new VelocityCommand(kickerSubsystem, 30)
+                .andThen(
+                        new ParallelCommandGroup(
+                                new VelocityCommand(indexerSubsystem, 15),
+                                new VelocityCommand(hopperSubsystem, 15)
+                        )
+                )
+        )).onFalse(
                 new ParallelCommandGroup(
                         new PositionCommand(turretSubsystem, 0),
                         new PositionCommand(hoodSubsystem, 0),
