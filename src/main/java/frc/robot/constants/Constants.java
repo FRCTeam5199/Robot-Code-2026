3a030d38439d0c27da47c3428ca27d3b6972848b
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2026-03-03 20:50:45 -0800

    added front limelight

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -31,5 +31,5 @@ public class Constants {
     public static final double TURRET_BUFFER_SIZE = 20d;
     public static final String LIMELIGHT_LEFT_NAME = "limelight-left"; //10.51.99.11:5801
     public static final String LIMELIGHT_RIGHT_NAME = "limelight-right"; //10.51.99.12:5801
-    public static final String LIMELIGHT_TURRET_NAME = "limelight-turret"; //10.51.99.13:5801 - NOT SET YET
+    public static final String LIMELIGHT_FRONT_NAME = "limelight-turret"; //10.51.99.13:5801 - NOT SET YET
 }
