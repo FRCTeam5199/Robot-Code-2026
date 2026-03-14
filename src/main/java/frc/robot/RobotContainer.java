707aa0b5371565cdb9950c0766c79b215a4ef9cf
Rohan-5199 <rohan745patel@gmail.com>
2026-03-14 11:36:06 -0700

    more changes before first qual

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -161,21 +161,21 @@ RobotContainer {
         stop = RobotCommands.stopClimb();
 
 
-//        leftBumperPressed = new SelectCommand<>(Map.ofEntries(
-//                Map.entry(Setpoint.HUB, new ParallelCommandGroup(
-//                        turretHub, hoodHub, shooterHub, kickerHub
-//                )),
-//                Map.entry(Setpoint.TOWER, new ParallelCommandGroup(
-//                        turretTower, hoodTower, shooterTower, kickerTower
-//                )),
-//                Map.entry(Setpoint.OUTPOST, new ParallelCommandGroup(
-//                        turretOutpost, hoodOutpost, shooterOutpost, kickerOutpost
-//                )),
-//                Map.entry(Setpoint.LEFT_CORNER, new ParallelCommandGroup(
-//                        turretLeftCorner, hoodLeftCorner, shooterLeftCorner, kickerLeftCorner
-//                ))
-//        ), RobotContainer::getCurrentSetpoint).alongWith(RobotCommands.indexBalls());
-//        leftBumperReleased = RobotCommands.idleState();
+       leftBumperPressed = new SelectCommand<>(Map.ofEntries(
+               Map.entry(Setpoint.HUB, new ParallelCommandGroup(
+                       turretHub, hoodHub, shooterHub, kickerHub
+               )),
+               Map.entry(Setpoint.TOWER, new ParallelCommandGroup(
+                       turretTower, hoodTower, shooterTower, kickerTower
+               )),
+               Map.entry(Setpoint.OUTPOST, new ParallelCommandGroup(
+                       turretOutpost, hoodOutpost, shooterOutpost, kickerOutpost
+               )),
+               Map.entry(Setpoint.LEFT_CORNER, new ParallelCommandGroup(
+                       turretLeftCorner, hoodLeftCorner, shooterLeftCorner, kickerLeftCorner
+               ))
+       ), RobotContainer::getCurrentSetpoint).alongWith(RobotCommands.indexBalls());
+       leftBumperReleased = RobotCommands.idleState();
 
         NamedCommands.registerCommand("shoot", leftTriggerPressed);
         NamedCommands.registerCommand("hoodZero", hoodZero);
@@ -455,20 +455,8 @@ RobotContainer {
         commandXboxController.leftTrigger().onTrue(leftTriggerPressed)
                 .onFalse(leftTriggerReleased);
 
-        // Back Up Setpoints
-        commandXboxController.leftBumper().onTrue(new SequentialCommandGroup(new VelocityCommand(intakeRollerSubsystem, -90), new VelocityCommand(hopperSubsystem, -60)))
-                .onFalse(new ParallelCommandGroup(new VelocityCommand(hopperSubsystem, HopperConstants.IDLING_SPEED), new VelocityCommand(intakeRollerSubsystem, 0)));
-
-        // commandXboxController.y().onTrue(new InstantCommand(() -> climberSubsystem.setVoltage(12)));
-        // commandXboxController.b().onTrue(new InstantCommand(() -> climberSubsystem.setVoltage(-12)));
-
-        //Auto climb Left
-        // commandXboxController.povLeft().onTrue(new PositionCommand(climberSubsystem, ClimberConstants.DEPLOY)
-        //                 .andThen(Autos.pidAlignLeft()))
-        //         .onFalse(commandSwerveDrivetrain.applyRequest(() -> drive
-        //                 .withVelocityX(-requestXVelocity)
-        //                 .withVelocityY(-requestYVelocity)
-        //                 .withRotationalRate(requestRotationalVelocity)));
+        commandXboxController.leftBumper().onTrue(new VelocityCommand(intakeRollerSubsystem, -116))
+                .onFalse(new VelocityCommand(intakeRollerSubsystem, 0));
 
         operatorCommandXboxController.y().onTrue(setHubSetpoint);
         operatorCommandXboxController.x().onTrue(setLeftCornerSetpoint);
@@ -478,13 +466,9 @@ RobotContainer {
         operatorCommandXboxController.povUp().onTrue(new InstantCommand(() -> shooterSubsystem.changeOffset(.5)));
         operatorCommandXboxController.povDown().onTrue(new InstantCommand(() -> shooterSubsystem.changeOffset(-.5)));
 
-        // operatorCommandXboxController.rightTrigger().onTrue(new InstantCommand(() -> climberSubsystem.setVoltage(-12)))
-        //         .onFalse(new InstantCommand(() -> climberSubsystem.setVoltage(0)));
-        // operatorCommandXboxController.leftTrigger().onTrue(new InstantCommand(() -> climberSubsystem.setVoltage(12)))
-        //         .onFalse(new InstantCommand(() -> climberSubsystem.setVoltage(0)));
-
         operatorCommandXboxController.rightBumper().onTrue(RobotCommands.outtake())
                 .onFalse(RobotCommands.idleState());
+        operatorCommandXboxController.leftBumper().onTrue(leftBumperPressed).onFalse(leftBumperReleased);
     }
 
     public Command getAutonomousCommand() {
