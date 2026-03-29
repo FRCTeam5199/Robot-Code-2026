ea971aea21c2fff5bb53e03ae0d6ae57a05c619e
Rohan-5199 <rohan745patel@gmail.com>
2026-03-28 21:35:53 -0700

    hi

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,7 +22,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.IntakeRollerSubsystem;
 import frc.robot.subsystems.templates.VelocityCommand;
 import frc.robot.utility.LimelightHelpers;
 
--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -19,7 +19,7 @@ public class Constants {
     public static final double HUB_RADIUS = .52959;
     public static final Transform2d ROBOT_TO_TURRET = new Transform2d(-.13335, .13335, new Rotation2d(0));
     public static final double PHASE_DELAY = 0.0;
-    public static final double ROTATIONAL_PHASE_DELAY = 0.1;
+    public static final double ROTATIONAL_PHASE_DELAY = 0.0;
     public static final double CENTER_TO_BUMPER = 0.69 / 2d; //TODO: do this with actual bumpers
     public static final Transform2d FRONT_LEFT_CORNER = new Transform2d(CENTER_TO_BUMPER, CENTER_TO_BUMPER, new Rotation2d(0));
     public static final Transform2d FRONT_RIGHT_CORNER = new Transform2d(CENTER_TO_BUMPER, -CENTER_TO_BUMPER, new Rotation2d(0));
