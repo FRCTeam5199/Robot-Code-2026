9da28da5bb3d6e92f1c37ab7047caa8e4b132066
Rohan-5199 <rohan745patel@gmail.com>
2026-04-03 10:18:04 -0700

    more changes before first qual

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -421,8 +421,8 @@ RobotContainer {
 //                )
 //        );
 
-        commandXboxController.rightBumper().onTrue(intakeAgitation)
-                .onFalse(new PositionCommand(intakePivotSubsystem, IntakePivotConstants.DEPLOY));
+        // commandXboxController.rightBumper().onTrue(intakeAgitation)
+        //         .onFalse(new PositionCommand(intakePivotSubsystem, IntakePivotConstants.DEPLOY));
 
         commandXboxController.rightTrigger().onTrue(intakeRollerIntake/*.alongWith(new VelocityCommand(hopperSubsystem, 50))*/)
                 .onFalse(intakeRollerStop/*.alongWith(new VelocityCommand(hopperSubsystem, 0))*/);
@@ -431,12 +431,12 @@ RobotContainer {
                 .onFalse(leftTriggerReleased);
 
 
-        commandXboxController.leftBumper().onTrue(new VelocityCommand(intakeRollerSubsystem, -116).alongWith(
-                        new VelocityCommand(hopperSubsystem, -90)
-                ))
-                .onFalse(new VelocityCommand(intakeRollerSubsystem, 0).alongWith(
-                        new VelocityCommand(hopperSubsystem, 0)
-                ));
+        // commandXboxController.leftBumper().onTrue(new VelocityCommand(intakeRollerSubsystem, -116).alongWith(
+        //                 new VelocityCommand(hopperSubsystem, -90)
+        //         ))
+        //         .onFalse(new VelocityCommand(intakeRollerSubsystem, 0).alongWith(
+        //                 new VelocityCommand(hopperSubsystem, 0)
+        //         ));
 
 //        operatorCommandXboxController.y().onTrue(setHubSetpoint);
 //        operatorCommandXboxController.x().onTrue(setLeftCornerSetpoint);
