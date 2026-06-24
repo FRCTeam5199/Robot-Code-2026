21e0d4f77ff1ebcf0ab5b15eb3b93332146760c5
Cydaguy <themrbestgamer@gmail.com>
2026-06-24 16:03:26 -0700

    Tested auto but still needs tweaking

--- src/main/java/frc/robot/Autos.java
+++ src/main/java/frc/robot/Autos.java
@@ -67,7 +67,7 @@ public final class Autos {
         redBottomDoubleTrench = new PathPlannerAuto("Red Bottom Double Trench");
         redTopDoubleTrench = new PathPlannerAuto("Red Top Double Trench");
 
-//        redBottomDoubleBump = new PathPlannerAuto("Red Bottom Double Bump");
+       redBottomDoubleBump = new PathPlannerAuto("Red Bottom Double Bump");
 //        redTopDoubleBump = new PathPlannerAuto("Red Top Double Bump");
 
         redBottomDelayedBump = new PathPlannerAuto("Red Bottom Delayed Bump");
@@ -85,7 +85,7 @@ public final class Autos {
 
         autonChooserRed.addOption("Red Left Double Trench", redBottomDoubleTrench);
         autonChooserRed.addOption("Red Right Double Trench", redTopDoubleTrench);
-//        autonChooserRed.addOption("Red Left Double Bump", redBottomDoubleBump);
+       autonChooserRed.addOption("Red Left Double Bump", redBottomDoubleBump);
 //        autonChooserRed.addOption("Red Right Double Bump", redTopDoubleBump);
         autonChooserRed.addOption("Red Left Delayed Bump", redBottomDelayedBump);
         autonChooserRed.addOption("Red Right Delayed Bump", redTopDelayedBump);
@@ -94,7 +94,7 @@ public final class Autos {
 
         autonChooserBlue.addOption("Blue Left Double Trench", redBottomDoubleTrench);
         autonChooserBlue.addOption("Blue Right Double Trench", redTopDoubleTrench);
-//        autonChooserBlue.addOption("Blue Left Double Bump", redBottomDoubleBump);
+       autonChooserBlue.addOption("Blue Left Double Bump", redBottomDoubleBump);
 //        autonChooserBlue.addOption("Blue Right Double Bump", redTopDoubleBump);
         autonChooserBlue.addOption("Blue Left Delayed Bump", redBottomDelayedBump);
         autonChooserBlue.addOption("Blue Right Delayed Bump", redTopDelayedBump);
