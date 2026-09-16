56d2741c099c884bd04a0d7f33a4cb4b074fbe5a
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2026-09-15 21:14:29 -0700

    final auton changes

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -18,9 +18,9 @@ public class Constants {
 
     public static final double HUB_RADIUS = .52959;
     public static final Transform2d ROBOT_TO_TURRET = new Transform2d(-.13335, .13335, new Rotation2d(0));
-    public static final double PHASE_DELAY = 0.05;
+    public static final double PHASE_DELAY = 0.035;
     public static final double ROTATIONAL_PHASE_DELAY = 0.05;
-    public static final double ACCELERATION_PHASE_DELAY = .02;
+    public static final double ACCELERATION_PHASE_DELAY = 0.02;
     public static final double CENTER_TO_BUMPER_LONG = 0.9271 / 2d;
     public static final double CENTER_TO_BUMPER_SHORT = 0.8128 / 2d;
     public static final Transform2d FRONT_LEFT_CORNER = new Transform2d(CENTER_TO_BUMPER_SHORT, CENTER_TO_BUMPER_LONG, new Rotation2d(0));
