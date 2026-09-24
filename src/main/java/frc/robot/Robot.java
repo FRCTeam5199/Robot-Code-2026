a77461fb5756d2560e8d5dce03be39f6a70046e5
Leo Sent <leonardosent@gmail.com>
2026-09-23 18:30:53 -0700

    Fixed merge issues

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,14 +20,6 @@ import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.utility.LimelightHelpers;
 import frc.robot.utility.ShotCalculator;
 
-import javax.xml.crypto.Data;
-import java.sql.Driver;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-import static frc.robot.subsystems.Vision.pendingMeasurements;
-
 public class Robot extends TimedRobot {
     public static final HoodSubsystem hoodSubsystem = HoodSubsystem.getInstance();
     public static final HopperSubsystem hopperSubsystem = HopperSubsystem.getInstance();
@@ -42,8 +34,7 @@ public class Robot extends TimedRobot {
     private final RobotContainer m_robotContainer;
     // private final UserInterface userInterface = UserInterface.getInstance();
     private Command m_autonomousCommand;
-    private static DriverStation.Alliance alliance;
-    private final List<Vision.VisionMeasurement> visionBatch = new ArrayList<>(3);
+    private static Alliance alliance;
 
     public Robot() {
         commandSwerveDrivetrain.configureAutoBuilder();
@@ -212,17 +203,7 @@ public class Robot extends TimedRobot {
         //     userInterface.setComponentData("Reset Voltage (F)", false);
         //     motorFollower.setVoltage(0);
         // }
-        visionBatch.clear();
-        Vision.VisionMeasurement m;
-        while ((m = pendingMeasurements.poll()) != null) {
-            visionBatch.add(m);
-        }
-        visionBatch.sort(Comparator.comparingDouble(Vision.VisionMeasurement::timestampSeconds));
 
-        for (Vision.VisionMeasurement measurement : visionBatch) {
-            commandSwerveDrivetrain.addVisionMeasurement(measurement.pose(), measurement.timestampSeconds(),
-                    VecBuilder.fill(measurement.xyStdev(), measurement.xyStdev(), 9999999999d));
-        }
         RobotContainer.periodic();
         CommandScheduler.getInstance().run();
 
@@ -233,9 +214,9 @@ public class Robot extends TimedRobot {
 
     @Override
     public void disabledInit() {
-        LimelightHelpers.SetThrottle("limelight-right", 2000);
-        LimelightHelpers.SetThrottle("limelight-left", 2000);
-        LimelightHelpers.SetThrottle("limelight-front", 2000);
+        LimelightHelpers.SetThrottle("limelight-right", 200);
+        LimelightHelpers.SetThrottle("limelight-left", 200);
+        LimelightHelpers.SetThrottle("limelight-front", 200);
         RobotContainer.setIsAutonomous(false);
     }
 
